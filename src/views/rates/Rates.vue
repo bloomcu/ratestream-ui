5d1d01588f72b706453327fd3c34711f957bdaef
Ryan Harmon <ryan@bloomcu.com>
2023-02-18 18:17:02 -0700

    Add empty state to rates view

--- src/views/rates/Rates.vue
+++ src/views/rates/Rates.vue
@@ -1,15 +1,21 @@
 <template>
   <LayoutDefault>
-    <div class="container flex items-center justify-between margin-y-md">
+    <div v-if="rateStore.rates.rates.length" class="container flex items-center justify-between margin-top-md">
       <h1 class="text-lg">Current rates</h1>
       <router-link :to="{name: 'rates-import'}" class="btn btn--primary">Edit rates</router-link>
     </div>
     
-    <div v-if="rateStore.rates" class="container margin-bottom-md">
-      <RateTable :rates="rateStore.rates.rates"/>
+    <div class="container margin-top-md">
+      <div v-if="rateStore.isLoading">Loading...</div>
+      <RateTable v-else-if="rateStore.rates.rates.length" :rates="rateStore.rates.rates"/>
+      <div v-else class="text-component padding-md radius-lg bg-primary bg-opacity-5%">
+        <h3>No rates</h3>
+        <p>Let's import a CSV.</p>
+        <router-link :to="{name: 'rates-import'}" class="btn btn--primary">Import CSV</router-link>
+      </div>
     </div>
     
-    <EditPromptModal/>
+    <!-- <EditPromptModal/> -->
   </LayoutDefault>
 </template>
 
@@ -18,7 +24,7 @@ import moment from "moment"
 import { ref, onMounted } from 'vue'
 import { useRateStore } from '@/domain/rates/store/useRateStore'
 import LayoutDefault from '@/app/layouts/LayoutDefault.vue'
-import EditPromptModal from '@/views/rates/modals/EditPromptModal.vue'
+// import EditPromptModal from '@/views/rates/modals/EditPromptModal.vue'
 import RateTable from '@/views/rates/components/RateTable.vue'
 
 const rateStore = useRateStore()
