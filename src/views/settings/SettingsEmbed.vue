ce177fe67ad501f72c8d21500497c98714b08dde
Ryan Harmon <ryan@bloomcu.com>
2023-02-20 16:21:35 -0700

    Add embed code to settings page

--- src/views/settings/SettingsEmbed.vue
+++ src/views/settings/SettingsEmbed.vue
@@ -1,26 +1,33 @@
 <template>
-  <LayoutDefault maxWidth="sm">
+  <LayoutDefault maxWidth="md">
     <div class="container margin-top-md">
       <h2>Settings</h2>
     </div>
     
     <div class="container margin-top-md">
       <div class="card card--shadow">
-        <div class="text-component border-bottom margin-bottom-md padding-bottom-md">
+        <div class="text-component">
           <!-- Embed code -->
           <h4>Your embed code</h4>
-          <p>[[ Insert instructions... ]]</p>
-          <div class="bg-darker padding-md radius-md">
-            [[ Insert embed code... ]]
+          <p>To install RateStream, place this script at the top of the <span class="text-bold color-primary">&lt;head&gt;</span> section just after any <span class="text-bold color-primary">&lt;meta&gt;</span> tags.</p>
+          <div v-if="organizationStore.organization" class="flex flex-column position-relative color-primary bg-primary bg-opacity-10% border border-primary border-opacity-60% margin-top-sm padding-md radius-md">
+            <span class="line-height-xl">
+              &lt;script&gt;window.STREAM_ORGANIZATION="{{ organizationStore.organization.slug }}"&lt;/script&gt; <br>
+              &lt;script async src="https://components.ratestream.io/assets/index.js"&gt;&lt;/script&gt;
+            </span>            
+
+            <ButtonCopyToClipboard :value="embedCode" class="btn btn--primary position-absolute top-xs right-xs">
+              Copy to clipboard
+            </ButtonCopyToClipboard>
           </div>
         </div>
         
-        <div class="text-component">
-          <h4>Detailed instruction or FAQs...</h4>
+        <!-- <div class="text-component">
+          <h4>How do I manually install this tracking script onto my site?</h4>
           <div>
             <p>[[ Insert detailed instructions or FAQ(s) ]]</p>
           </div>
-        </div>
+        </div> -->
       </div>
     </div>
   </LayoutDefault>
@@ -28,17 +35,18 @@
 
 <script setup>
 import moment from "moment"
-import { onMounted } from 'vue'
-// import { useOrganizationStore } from '@/domain/base/organizations/store/useOrganizationStore'
+import { computed, onMounted } from 'vue'
+import { useOrganizationStore } from '@/domain/base/organizations/store/useOrganizationStore'
 import LayoutDefault from '@/app/layouts/LayoutDefault.vue'
-// import UserAvatar from '@/domain/base/users/components/UserAvatar.vue'
-// import AppChip from '@/app/components/base/chips/AppChip.vue'
-// import ButtonDestroy from '@/app/components/base/buttons/ButtonDestroy.vue'
-// import ButtonCopyToClipboard from '@/app/components/base/buttons/ButtonCopyToClipboard.vue'
+import ButtonCopyToClipboard from '@/app/components/base/buttons/ButtonCopyToClipboard.vue'
 
-// const organizationStore = useOrganizationStore()
+const organizationStore = useOrganizationStore()
+
+let embedCode = computed(() => {
+  return `<script>window.STREAM_ORGANIZATION=\"${organizationStore.organization.slug}\"<\/script>\r\n<script src=\"https:\/\/components.ratestream.io\/assets\/index.js\"><\/script>`
+})
 
 onMounted(() => {
-  // organizationStore.show()
+  organizationStore.show()
 })
 </script>
