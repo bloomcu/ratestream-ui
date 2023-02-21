d915dd79ade567cce584d156408c246e4168d8aa
Ryan Harmon <ryan@bloomcu.com>
2023-02-20 20:37:05 -0700

    Add "Preview" to upload section

--- src/views/rates/components/Uploader.vue
+++ src/views/rates/components/Uploader.vue
@@ -52,17 +52,23 @@
         </div>
       </div> -->
       
-      <div v-if="fileStore.file" class="flex gap-sm justify-end items-center margin-y-sm">
-        <!-- Error -->
+      <!-- Error -->
+      <div v-if="fileStore.file">
         <div v-if="fileStore.file.errors" class="color-error bg-error bg-opacity-10% border border-error border-opacity-30% padding-xs radius-lg flex-grow">
           {{ fileStore.file.errors.uid[0] }}
         </div>
+      </div>
+      
+      <div v-if="fileStore.file" class="flex gap-sm justify-between items-center margin-y-sm">
+        <h3>Preview</h3>
         
-        <!-- Compare -->
-        <a v-if="rateStore.rates && rateStore.rates.rates.length" @click.prevent="compare()" href="" class="color-contrast-high">Compare to current rates</a>
-        
-        <!-- Publish -->
-        <button @click="rateStore.togglePublishPromptModal()" :disabled="fileStore.file.errors" class="btn btn--primary">Publish</button>
+        <div class="flex gap-md items-center">
+          <!-- Compare -->
+          <a v-if="rateStore.rates && rateStore.rates.rates.length" @click.prevent="compare()" href="" class="color-contrast-high">Compare to current rates</a>
+          
+          <!-- Publish -->
+          <button @click="rateStore.togglePublishPromptModal()" :disabled="fileStore.file.errors" class="btn btn--primary">Publish</button>
+        </div>
       </div>
       
       <!-- Table preview -->
