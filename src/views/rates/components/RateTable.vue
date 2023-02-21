988416cd45e986dad673cd229697ab027266586f
Tray Northern <traynorthern@yahoo.com>
2023-02-20 21:34:26 -0600

    Copy draft

--- src/views/rates/components/RateTable.vue
+++ src/views/rates/components/RateTable.vue
@@ -10,8 +10,19 @@
       <tr v-for="(row, index) in rows" :key="index" class="table__row">
         <td class="table__cell font-semibold">{{ row.uid }}</td>
         <td v-for="(column, index) in columns" :key="index" class="table__cell">
-          <ButtonCopyToClipboard :value="`<stream-cell uid=&quot;${row.uid}&quot; col=&quot;${column}&quot;></stream-cell>`">
-            {{ row.columns[column] }}</ButtonCopyToClipboard>
+          <VMenu  placement="top" :delay="{
+            hide: 50
+          }">
+            <span>
+              {{ row.columns[column] }}
+            </span>
+            <template #popper>
+              <div @click.prevent="copy(row.uid, column, index)" class="table-cell-inner">
+                <IconEmbed v-if="!isCopied.includes(`${index}${column}${index}`)" size="md" />
+                <span v-else>Copied!</span>
+              </div>
+            </template>
+          </VMenu>
         </td>
       </tr>
     </tbody>
@@ -20,6 +31,9 @@
 
 <script setup>
 import ButtonCopyToClipboard from '../../../app/components/base/buttons/ButtonCopyToClipboard.vue'
+import IconEmbed from '../../../app/components/base/icons/IconEmbed.vue'
+import useClipboard from '@/app/composables/base/useClipboard'
+import { ref } from 'vue'
 const props = defineProps({
   columns: {
     type: Array
@@ -28,4 +42,23 @@ const props = defineProps({
     type: Array
   }
 })
+const { copyToClipboard } = useClipboard()
+const isCopied = ref([])
+const copy = (uid, column, index) => {
+  copyToClipboard(`<stream-cell uid=&quot;${uid}&quot; col=&quot;${column}&quot;></stream-cell>`)
+  isCopied.value.push(`${index}${column}${index}`)
+}
 </script>
+
+<style lang="scss" scoped>
+.table__cell-embed-box {
+  background-color: black;
+  color: white;
+  height: 40px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
+}
+</style>
