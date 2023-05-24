234922a9bb18e93dbe6c400018d17e8d9dc5e739
Ryan Harmon <ryan@bloomcu.com>
2023-05-24 13:32:25 -0600

    Update rate table to deal with column objects

--- src/views/rates/components/RateTable.vue
+++ src/views/rates/components/RateTable.vue
@@ -3,14 +3,14 @@
     <thead class="table__header">
       <tr class="table__row">
         <th class="table__cell font-semibold" scope="col">Unique ID</th>
-        <th v-for="(column, index) in columns" :key="index" class="table__cell font-medium" scope="col">{{ column }}</th>
+        <th v-for="column in columns" :key="column.id" class="table__cell font-medium" scope="col">{{ column.name }}</th>
       </tr>
     </thead>
     <tbody class="table__body">
       <tr v-for="(row, index) in rows" :key="index" class="table__row">
         <td class="table__cell font-semibold">{{ row.uid }}</td>
         <td v-for="(column, index) in columns" :key="index" class="table__cell">
-          <CopyableTableCell :row="row" :column="column" />
+          <CopyableTableCell :row="row" :column="column.name" />
         </td>
       </tr>
     </tbody>
